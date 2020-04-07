a926dca6a9cfa595f1c5f5b80c660e4338782415
Oğuzhan Göller <goller.oguzhan@gmail.com>
2020-04-07 14:41:30 +0300

    Initial commit

---
+++ lib/sidekiq/compress.rb
@@ -0,0 +1,8 @@
+require "sidekiq/compress/version"
+
+module Sidekiq
+  module Compress
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/compress/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Compress
+    VERSION = "0.1.0"
+  end
+end
